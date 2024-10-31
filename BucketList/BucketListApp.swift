0ef997b45df90f0c8121cb5e9823680bbdc8f88a
Jatin Singh <singhjatin1812@gmail.com>
2024-11-01 01:00:33 +0530

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Jatin Singh on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketListTests/BucketListTests.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListTests.swift
+//  BucketListTests
+//
+//  Created by Jatin Singh on 25/10/24.
+//
+
+import Testing
+@testable import BucketList
+
+struct BucketListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
