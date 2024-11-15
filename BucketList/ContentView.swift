08afca5d40cab6c0f24dd69b6e3e1ac2da1bda5d
Jatin Singh <singhjatin1812@gmail.com>
2024-11-15 10:36:27 +0530

    selecting and editing map annotations

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -14,18 +14,22 @@ struct ContentView: View {
         span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)))
     
     @State private var locations = [Location]()
+    @State private var selectedPlace: Location?
     
     var body: some View {
         MapReader { proxy in
             Map(initialPosition: startPosition){
                 ForEach(locations) {location in
-                    Annotation(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
+                    Annotation(location.name, coordinate: location.coordinate) {
                         Image(systemName: "star.circle")
                             .resizable()
                             .foregroundStyle(.red)
                             .frame(width: 44, height: 44)
                             .background(.white)
                             .clipShape(.circle)
+                            .onLongPressGesture {
+                                selectedPlace = location
+                            }
                     }
                 }
             }
@@ -33,6 +37,13 @@ struct ContentView: View {
                     if let coordinate = proxy.convert(position, from: .local) {
                         let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
                         locations.append(newLocation)
+                    }
+                }
+                .sheet(item: $selectedPlace){ place in //this place is unwrapped one
+                    EditView(location: place) { newLocation in
+                        if let index = locations.firstIndex(of: place) {
+                            locations[index] = newLocation
+                        }
                     }
                 }
         }
